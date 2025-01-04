35df8940a05354d93f77afa50ae49a55573048fd
SaRa-Harahsheh <saraharahsheh0@gmail.com>
2025-01-05 00:34:01 +0300

    task3java

--- task3.js/task3.js
+++ task3.js/task3.js
@@ -74,3 +74,122 @@ function greet(name) {
     
 }
 greet("sara");
+
+
+
+/*
+5
+what is the error:
+function double(cat) {
+  return 2 * x;
+}
+
+function double(7) {
+  return 2 * 7;
+}
+
+function double('7') {
+  return 2 * 'x';
+}
+*/
+
+
+
+function double(cat) {
+    
+  return 2 * cat;
+}
+
+function double(x) {
+  return 2 * 7;
+}
+
+function double(y) {
+  return 2 * y ;
+}
+
+
+
+/*
+6
+fix these functions:
+func double1(x {
+  return 2 * x ;
+}
+
+functiondouble2 x)
+return 2 * x;
+}
+
+function (x) double3 {
+  return 2 * x;
+
+*/
+
+
+
+function double1(x) {
+  return 2 * x ;
+}
+
+function double2 (x){
+return 2 * x;
+}
+
+function  double3(x) {
+  return 2 * x;
+}
+
+
+/*
+7
+Write a function called cube that:
+accept 1 parameter and calculate the cube of this number
+
+Ex: cube(4)
+=> 64
+*/
+
+function cube(number){
+console.log(number**3)
+}
+cube(4);
+
+
+
+/*
+8
+Write a function called multiply that:
+accept 2 parameters and calculate the multiply of these 2 numbers
+
+Ex: multiply(3,4)
+=> 12
+Ex: multiply(5,4)
+=> 20
+*/
+
+function multiply(num1,num2) {
+ console.log(num1*num2)  
+}
+
+multiply(3,4);
+multiply(5,4);
+
+
+/*
+9
+Write a function called canIGetADrivingLicense that:
+accept 1 parameter represent the age
+and if the age greater than or equal to 20 return "yes you can"
+otherwise return "please come back after X years to get one"
+
+Ex: canIGetADrivingLicense(21)
+=> "yes you can"
+
+Ex: canIGetADrivingLicense(17)
+=> "please come back after 3 years to get one"
+
+Ex: canIGetADrivingLicense(20)
+=> "yes you can"
+
+*/
